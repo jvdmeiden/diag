06ae66ff9fa2054b6855a902b3ae011cad610290
Jan van der Meiden <jvdmeiden@gmail.com>
2024-09-06 19:24:55 +0200

    'Straight' version working

--- diag.js
+++ diag.js
@@ -31,42 +31,76 @@ function submitChoice(inp){
   while (svgObject.firstChild) {
     svgObject.removeChild(svgObject.firstChild);
   }
-  while (x <= maxX){
-    y = 0;
-    while (y <= maxY){
-      if (Math.random() < 0.5){
-        newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-        newLine.setAttribute("x1",x);
-        newLine.setAttribute("y1",y);
-        newLine.setAttribute("x2",x+blockSize);
-        if (diagBox.checked == true) {
-	  newLine.setAttribute("y2",y+blockSize);
-	} else {
-	  newLine.setAttribute("y2",y);
-	} 
-        newLine.setAttribute("stroke-width",lineWidth);
-        newLine.setAttribute("stroke","black");
-        newLine.setAttribute("stroke-linecap","round");
-        svgObject.appendChild(newLine);
-      } else {
-        newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-        newLine.setAttribute("x1",x);
-        newLine.setAttribute("y1",y+blockSize);
-        if (diagBox.checked == true) {
+  if (diagBox.checked == true) {
+    while (x <= maxX){
+      y = 0;
+      while (y <= maxY){
+        if (Math.random() < 0.5){
+          newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+          newLine.setAttribute("x1",x);
+          newLine.setAttribute("y1",y);
           newLine.setAttribute("x2",x+blockSize);
-	} else {
-	  newLine.setAttribute("x2",x);
-	} 
-        newLine.setAttribute("y2",y);
-        newLine.setAttribute("stroke-width",lineWidth);
-        newLine.setAttribute("stroke","black");
-        newLine.setAttribute("stroke-linecap","round");
-        svgObject.appendChild(newLine);
-      } 
-      y += blockSize;
-	    console.log(angle);
+	  newLine.setAttribute("y2",y+blockSize);
+          newLine.setAttribute("stroke-width",lineWidth);
+          newLine.setAttribute("stroke","black");
+          newLine.setAttribute("stroke-linecap","round");
+          svgObject.appendChild(newLine);
+        } else {
+          newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+          newLine.setAttribute("x1",x);
+          newLine.setAttribute("y1",y+blockSize);
+          newLine.setAttribute("x2",x+blockSize);
+          newLine.setAttribute("y2",y);
+          newLine.setAttribute("stroke-width",lineWidth);
+          newLine.setAttribute("stroke","black");
+          newLine.setAttribute("stroke-linecap","round");
+          svgObject.appendChild(newLine);
+        } 
+        y += blockSize;
+      }
+      x += blockSize;
     }
-    x += blockSize;
+  } else {
+    inc = Math.sqrt(2)*blockSize/2;
+    console.log(inc);
+    n = 0;
+    m = 0;
+    x = -inc;
+    while (x <= maxX + inc){
+      n = 0;
+      y = -inc;
+      while (y <= maxY + inc){
+        console.log("fdsfdsdfsdfs  ");
+        console.log(n + "  " + m);
+        if ( (n + m) % 2 == 0){
+          if (Math.random() < 0.5){
+            newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+            newLine.setAttribute("x1",x-inc);
+            newLine.setAttribute("y1",y);
+            newLine.setAttribute("x2",x+inc);
+            newLine.setAttribute("y2",y);
+            newLine.setAttribute("stroke-width",lineWidth);
+            newLine.setAttribute("stroke","black");
+            newLine.setAttribute("stroke-linecap","round");
+            svgObject.appendChild(newLine);
+          } else {
+            newLine = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+            newLine.setAttribute("x1",x);
+            newLine.setAttribute("y1",y-inc);
+            newLine.setAttribute("x2",x);
+            newLine.setAttribute("y2",y+inc);
+            newLine.setAttribute("stroke-width",lineWidth);
+            newLine.setAttribute("stroke","black");
+            newLine.setAttribute("stroke-linecap","round");
+            svgObject.appendChild(newLine);
+          } 
+        } 
+        y += inc;
+        n += 1;
+      }
+      x += inc;
+      m += 1;
+    }
   }
   return(false);
 }
@@ -81,4 +115,5 @@ function getQueryVariable(variable)
   }
   return(false);
 }
+
 
